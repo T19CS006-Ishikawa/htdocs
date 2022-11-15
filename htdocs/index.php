53c6b6840c7a9783c5540a111549283393df6012
T19CS006-Ishikawa <takumi13226@172.24.91.181>
2022-11-15 10:57:49 +0900

    Composer_Inからindex.phpをコピー＋line bot sdkのインストール

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,54 @@
 <?php
+/*
+ //Composerでインストールしたライブラリを一括読み込み
+ require_once __DIR__ . '/vendor/autoload.php';
+ 
+ //アクセストークンを使いCurlHTTPClientをインスタンス化
+ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+ 
+ //CurlHTTPClientとシークレットを使い、LINE Botをインスタンス化
+ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+ 
+ //LINE Message API がリクエストに付与した署名を取得
+ $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+ 
+ //署名が正当かチェック。正当であればリクエストをパースし配列へ
+ $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+ 
+ foreach ($events as $event){
+ //テキストを返信
+ $bot->replyText($event -> getReplyToken(),'TextMessage');
+ }
+ 
+ */
 
-    echo "This is from [htdocs] repository";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$input = file_get_contents('php://input');
+$json = json_decode($input);
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('DzNOTsY/Ht98S9QxPPUNcjN5m85UvSY1lEpZyfyLN+RFcuUs2laRz664s33dV/DYK+Q1b4COWuJTxhs/NBc0KcTQG4Fe52Gz+rsdAWSLbFQ8m8FAHoP/gU7lwCg5vZlggPAQOZAkKOBa7iDDAc+xNAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '9dd5b0db9baa14fcfb44558627c6fbeb']);
+
+$event = $json->events[0];
+if($event->type == 'message') {
+    $messageData = $event->message;
+    if($messageData->type == 'text') {
+        if($messageData->text == 'ガブリエル') {
+            $replyText = 'ジェズス';
+        } else if($messageData->text == 'マルティン') {
+            $replyText = 'ウーデゴール';
+        } else {
+            $replyText = $messageData->text;
+        }
+    } else if($messageData->type == 'image') {
+        $replyText = '画像';
+    } else {
+        $replyText = "テキスト・画像以外";
+    }
+}
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($replyText);
+$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+return;
 ?>
---
+++ htdocs/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e17c4fcef81158b3dfbcba3822e65de84298ef5c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e17c4fcef81158b3dfbcba3822e65de84298ef5c',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '7.5.0',
+            'version' => '7.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => '441408e5f4273c733e4559691f5dfe48f072c38f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
